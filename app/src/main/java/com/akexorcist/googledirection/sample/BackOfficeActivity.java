6cc6bc544839baa7bc3b8a8dff1b6f893733d281
masterUNG <phrombutr@gmail.com>
2017-03-22 16:04:26 +0700

    Finish 22March

--- app/src/main/java/com/akexorcist/googledirection/sample/BackOfficeActivity.java
+++ app/src/main/java/com/akexorcist/googledirection/sample/BackOfficeActivity.java
@@ -2,12 +2,30 @@ package com.akexorcist.googledirection.sample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class BackOfficeActivity extends AppCompatActivity {
+public class BackOfficeActivity extends AppCompatActivity implements View.OnClickListener {
+
+    //Explicit
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_back_office);
+
+        //Initial View
+        button = (Button) findViewById(R.id.btnBackOffice);
+
+        //Button Controller
+        button.setOnClickListener(BackOfficeActivity.this);
+
+
+    }   // Main Method
+
+    @Override
+    public void onClick(View view) {
+
     }
-}
+}   // Main Class
